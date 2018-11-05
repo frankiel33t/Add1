55df077502c418d0dccb25b8dcfd1133d73ce2ef
Моргун Владислав <frankieappstore@gmail.com>
2018-11-05 18:06:52 +0200

    added some new functions

--- Add1/MainViewController.swift
+++ Add1/MainViewController.swift
@@ -9,11 +9,77 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var numbersLabel:UILabel?
+    @IBOutlet weak var scoreLabel:UILabel?
+    @IBOutlet weak var inputField:UITextField?
+    
+    var score:Int = 0
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
+        
+        inputField?.addTarget(self, action: #selector(textFieldDidChange(textField:)), for:UIControl.Event.editingChanged)
 
         // Do any additional setup after loading the view.
+    }
+    
+    func updateScoreLabel() {
+        scoreLabel?.text = "\(score)"
+    }
+    
+    func setRandomNumberLabel() {
+        numbersLabel?.text = generateRandomNumber()
+    }
+    
+    @objc func textFieldDidChange(textField:UITextField)
+    {
+        if inputField?.text?.characters.count ?? 0 < 4
+        {
+            return
+        }
+        
+        if  let numbers_text    = numbersLabel?.text,
+            let input_text      = inputField?.text,
+            let numbers = Int(numbers_text),
+            let input   = Int(input_text)
+        {
+            print("Comparing: \(input_text) minus \(numbers_text) == \(input - numbers)")
+            
+            if(input - numbers == 1111)
+            {
+                print("Correct!")
+                
+                score += 1
+            }
+            else
+            {
+                print("Incorrect!")
+                
+                score -= 1
+            }
+        }
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
+    }
+    
+    func generateRandomNumber() -> String
+    {
+        var result:String = ""
+        
+        for _ in 1...4
+        {
+            let digit = Int.random(in: 1..<9)
+            
+            result += "\(digit)"
+        }
+        
+        return result
     }
 
 
